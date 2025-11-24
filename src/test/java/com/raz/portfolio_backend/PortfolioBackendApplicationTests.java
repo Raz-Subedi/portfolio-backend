1b01fcf337ea4f12c0b324e1d708828dead972dc
Raz Subedi <subedirazkumar@gmail.com>
2025-11-24 21:51:23 +0545

    Initial Commit

---
+++ src/test/java/com/raz/portfolio_backend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raz.portfolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
